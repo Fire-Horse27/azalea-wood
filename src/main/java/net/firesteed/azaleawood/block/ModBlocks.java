6e4278443a0191ea8c53358ac4ade6b021ce96ff
Fire-Horse27 <firehorse2727@gmail.com>
2023-09-04 14:21:14 -0400

    Added distinct map colors

--- src/main/java/net/firesteed/azaleawood/block/ModBlocks.java
+++ src/main/java/net/firesteed/azaleawood/block/ModBlocks.java
@@ -12,10 +12,10 @@ import net.minecraft.block.*;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.resource.featuretoggle.FeatureFlag;
 import net.minecraft.resource.featuretoggle.FeatureFlags;
-import net.minecraft.sound.SoundEvents;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 
 public class ModBlocks {
     /*this.name = string;
@@ -35,53 +35,67 @@ public class ModBlocks {
             ItemGroups.BUILDING_BLOCKS);
     public static final Block AZALEA_PRESSURE_PLATE = registerBlock("azalea_pressure_plate",
             new PressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING,
-                    FabricBlockSettings.copy(Blocks.OAK_PRESSURE_PLATE), AZALEA), ItemGroups.BUILDING_BLOCKS);
+                    FabricBlockSettings.copy(Blocks.OAK_PRESSURE_PLATE).mapColor(MapColor.TERRACOTTA_GRAY),
+                    AZALEA), ItemGroups.BUILDING_BLOCKS);
 
     public static final Block AZALEA_TRAPDOOR = registerBlock("azalea_trapdoor",
-            new TrapdoorBlock(FabricBlockSettings.copy(Blocks.OAK_TRAPDOOR), AZALEA), ItemGroups.BUILDING_BLOCKS);
+            new TrapdoorBlock(FabricBlockSettings.copy(Blocks.OAK_TRAPDOOR).mapColor(MapColor.TERRACOTTA_GRAY)
+                    , AZALEA), ItemGroups.BUILDING_BLOCKS);
     public static final Block AZALEA_DOOR = registerBlock("azalea_door",
-            new DoorBlock(FabricBlockSettings.copy(Blocks.OAK_DOOR), AZALEA), ItemGroups.BUILDING_BLOCKS);
+            new DoorBlock(FabricBlockSettings.copy(Blocks.OAK_DOOR).mapColor(MapColor.TERRACOTTA_GRAY)
+                    , AZALEA), ItemGroups.BUILDING_BLOCKS);
 
     public static final WoodType AZALEA_TYPE = new WoodType("azalea", AZALEA);
     public static final Block AZALEA_FENCE_GATE = registerBlock("azalea_fence_gate",
-            new FenceGateBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE_GATE), AZALEA_TYPE), ItemGroups.BUILDING_BLOCKS);
+            new FenceGateBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE_GATE).mapColor(MapColor.TERRACOTTA_GRAY)
+                    , AZALEA_TYPE), ItemGroups.BUILDING_BLOCKS);
     public static final Block AZALEA_FENCE = registerBlock("azalea_fence",
-            new FenceBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE)), ItemGroups.BUILDING_BLOCKS);
+            new FenceBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE).mapColor(MapColor.TERRACOTTA_GRAY)
+            ), ItemGroups.BUILDING_BLOCKS);
 
     public static final Block AZALEA_PLANKS = registerBlock("azalea_planks",
-            new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS)), ItemGroups.BUILDING_BLOCKS);
+            new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).mapColor(MapColor.TERRACOTTA_GRAY)
+            ), ItemGroups.BUILDING_BLOCKS);
     public static final Block AZALEA_SLAB = registerBlock("azalea_slab",
-            new SlabBlock(FabricBlockSettings.copy(Blocks.OAK_SLAB)), ItemGroups.BUILDING_BLOCKS);
+            new SlabBlock(FabricBlockSettings.copy(Blocks.OAK_SLAB).mapColor(MapColor.TERRACOTTA_GRAY)
+            ), ItemGroups.BUILDING_BLOCKS);
     public static final Block AZALEA_STAIRS = registerBlock("azalea_stairs",
-            new StairsBlock(AZALEA_PLANKS.getDefaultState(), FabricBlockSettings.copy(Blocks.OAK_STAIRS)),
+            new StairsBlock(AZALEA_PLANKS.getDefaultState(), FabricBlockSettings.copy(Blocks.OAK_STAIRS)
+                    .mapColor(MapColor.TERRACOTTA_GRAY)),
             ItemGroups.BUILDING_BLOCKS);
 
     public static final Block STRIPPED_AZALEA_WOOD = registerBlock("stripped_azalea_wood",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD)), ItemGroups.BUILDING_BLOCKS);
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD).mapColor(MapColor.TERRACOTTA_GRAY)),
+            ItemGroups.BUILDING_BLOCKS);
     public static final Block STRIPPED_AZALEA_LOG = registerBlock("stripped_azalea_log",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_LOG)), ItemGroups.BUILDING_BLOCKS);
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_LOG).mapColor(MapColor.TERRACOTTA_GRAY)),
+            ItemGroups.BUILDING_BLOCKS);
     public static final Block AZALEA_WOOD = registerBlock("azalea_wood",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD)), ItemGroups.BUILDING_BLOCKS);
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD).mapColor(MapColor.RAW_IRON_PINK)),
+            ItemGroups.BUILDING_BLOCKS);
     public static final Block AZALEA_LOG = registerBlock("azalea_log",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG)), ItemGroups.BUILDING_BLOCKS);
+            new PillarBlock(AbstractBlock.Settings.of(Material.WOOD, (state) -> {
+                return state.get(PillarBlock.AXIS) == Direction.Axis.Y ? MapColor.TERRACOTTA_GRAY : MapColor.RAW_IRON_PINK;
+            }).strength(2.0F).sounds(BlockSoundGroup.WOOD)), ItemGroups.BUILDING_BLOCKS);
 
     public static final TerraformSignBlock AZALEA_SIGN = registerSignBlock("azalea_sign", new TerraformSignBlock(
             new Identifier(AzaleaWood.MOD_ID, "entity/signs/azalea"),
-            FabricBlockSettings.copyOf(Blocks.OAK_SIGN)));
+            FabricBlockSettings.copyOf(Blocks.OAK_SIGN).mapColor(MapColor.TERRACOTTA_GRAY)));
     public static final TerraformWallSignBlock AZALEA_WALL_SIGN = registerWallSignBlock("azalea_wall_sign",
             new TerraformWallSignBlock(new Identifier(AzaleaWood.MOD_ID, "entity/signs/azalea"),
-            FabricBlockSettings.copyOf(Blocks.OAK_WALL_SIGN).dropsLike(AZALEA_SIGN)));
+            FabricBlockSettings.copyOf(Blocks.OAK_WALL_SIGN).dropsLike(AZALEA_SIGN).mapColor(MapColor.TERRACOTTA_GRAY)));
     public static final TerraformHangingSignBlock AZALEA_HANGING_SIGN = registerHangingSignBlock(
             "azalea_hanging_sign", new TerraformHangingSignBlock(
             new Identifier(AzaleaWood.MOD_ID, "entity/signs/hanging/azalea"),
             new Identifier(AzaleaWood.MOD_ID, "textures/gui/hanging_signs/azalea"),
-            FabricBlockSettings.copyOf(Blocks.OAK_HANGING_SIGN).requires(FeatureFlags.UPDATE_1_20)));
+            FabricBlockSettings.copyOf(Blocks.OAK_HANGING_SIGN).requires(FeatureFlags.UPDATE_1_20)
+                    .mapColor(MapColor.TERRACOTTA_GRAY)));
     public static final TerraformWallHangingSignBlock AZALEA_WALL_HANGING_SIGN = registerWallHangingSignBlock(
             "azalea_wall_hanging_sign", new TerraformWallHangingSignBlock(
             new Identifier(AzaleaWood.MOD_ID, "entity/signs/hanging/azalea"),
             new Identifier(AzaleaWood.MOD_ID, "textures/gui/hanging_signs/azalea"),
             FabricBlockSettings.copyOf(Blocks.OAK_WALL_HANGING_SIGN).dropsLike(AZALEA_HANGING_SIGN)
-                    .requires(FeatureFlags.UPDATE_1_20)));
+                    .requires(FeatureFlags.UPDATE_1_20).mapColor(MapColor.TERRACOTTA_GRAY)));
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
