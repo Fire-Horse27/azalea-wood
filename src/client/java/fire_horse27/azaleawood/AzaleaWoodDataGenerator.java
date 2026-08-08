b1123af310ad180be27ab9b6009d3a94cd78cd10
Fire-Horse27 <firehorse2727@gmail.com>
2026-08-08 18:48:36 -0500

    Fixed removed some pointless datagen

--- src/client/java/fire_horse27/azaleawood/AzaleaWoodDataGenerator.java
+++ src/client/java/fire_horse27/azaleawood/AzaleaWoodDataGenerator.java
@@ -3,9 +3,6 @@ package fire_horse27.azaleawood;
 import fire_horse27.azaleawood.datagen.ModModelProvider;
 import fire_horse27.azaleawood.datagen.ModRecipeProvider;
 import fire_horse27.azaleawood.datagen.ModBlockLootSubProvider;
-//import fire_horse27.azaleawood.datagen.ModBlockTagsProvider;
-import fire_horse27.azaleawood.datagen.ModEntityTypeTagsProvider;
-//import fire_horse27.azaleawood.datagen.ModItemTagsProvider;
 //import fire_horse27.azaleawood.datagen.ModVillagerTradeKeyTagProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -15,9 +12,6 @@ public class AzaleaWoodDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(ModBlockLootSubProvider::new);
-		//pack.addProvider(ModBlockTagsProvider::new);
-		pack.addProvider(ModEntityTypeTagsProvider::new);
-		//pack.addProvider(ModItemTagsProvider::new);
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
 		//pack.addProvider(ModVillagerTradeKeyTagProvider::new);
